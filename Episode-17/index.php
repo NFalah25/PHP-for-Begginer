d5b6f9a3bd54e1237c83905e29e1a2ca416ec7d9
= Nfalah25 <= falahnaufal1@gmail.com>
2023-03-25 19:54:11 +0700

    Episode-17 PHP Data Objects

---
+++ Episode-17/index.php
@@ -0,0 +1,18 @@
+<?php 
+require 'function.php';
+// require 'router.php';
+
+// connect to our database
+$dsn = "mysql:host=localhost;port=3306;dbname=myapp;user=root;charset=utf8mb4";
+
+$pdo = new PDO($dsn);
+
+$statement = $pdo -> prepare("SELECT*FROM posts");
+$statement -> execute();
+
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+foreach($posts as $post){
+    echo "<li>" . $post['title'] . "</li>";
+}
+
+?>
